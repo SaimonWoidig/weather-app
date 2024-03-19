895ee5986d20e1976c1e90c9c173c5110c3a8b7a
Šimon Woidig <woidig.simon@gmail.com>
2024-03-19 01:15:36 +0100

    added sample FE code for codecov

---
+++ frontend/src/services/weather-api/sample-service.ts
@@ -0,0 +1,5 @@
+function sample(num: number): number {
+    return num
+}
+
+export {sample}
---
+++ frontend/test/services/sample-service.spec.ts
@@ -0,0 +1,12 @@
+import {expect, test} from 'vitest';
+import {sample} from "../../src/services/weather-api/sample-service.ts";
+
+test("sample service test", () => {
+    const input = 2
+
+    const expected = 2
+
+    const actual = sample(input)
+
+    expect(expected).to.eq(actual)
+})
