f11a9cb63353c2da6c19735f2d2092a4045a46ab
Šimon Woidig <woidig.simon@gmail.com>
2024-04-11 01:13:36 +0200

    feat: use downstream library constants

--- backend/src/main/java/cz/woidig/backend/utils/id/NanoIDGenerator.java
+++ backend/src/main/java/cz/woidig/backend/utils/id/NanoIDGenerator.java
@@ -7,24 +7,16 @@ import java.util.Random;
 
 @AllArgsConstructor
 public final class NanoIDGenerator implements IDGenerator {
-    public static final char[] DEFAULT_ALPHABET = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            '-', '_'
-    };
-    public static final int DEFAULT_LENGTH = 12;
-
     private final Random random;
     private final char[] alphabet;
     private final int idLength;
 
     public NanoIDGenerator(Random random, int idLength) {
-        this(random, DEFAULT_ALPHABET, idLength);
+        this(random, NanoIdUtils.DEFAULT_ALPHABET, idLength);
     }
 
     public NanoIDGenerator(Random random) {
-        this(random, DEFAULT_LENGTH);
+        this(random, NanoIdUtils.DEFAULT_SIZE);
     }
 
     @Override
