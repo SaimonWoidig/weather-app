ce414d0a3a3d6ec1f1659e1dd4b6bb214ee24698
Šimon Woidig <woidig.simon@gmail.com>
2024-05-20 19:21:35 +0200

    feat: tests

--- backend/src/test/java/cz/woidig/backend/service/user/UserServiceTest.java
+++ backend/src/test/java/cz/woidig/backend/service/user/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
@@ -21,11 +22,14 @@ class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     private UserService userService;
 
     @BeforeEach
     void setUp() {
-        userService = new UserService(userRepository);
+        userService = new UserService(userRepository, passwordEncoder);
     }
 
     @Test
@@ -45,5 +49,28 @@ class UserServiceTest {
         Mockito.when(userRepository.findUserByUserId(Mockito.anyString()))
                 .thenReturn(Optional.empty());
         assertThrows(UsernameNotFoundException.class, () -> userService.getUser(Mockito.anyString()));
+    }
+
+    @Test
+    void test_changePassword_success() {
+        String userId = "userId";
+        String newPassword = "newPassword";
+        User user = new User(userId, "email", "oldPasswordHash");
+        Mockito.when(userRepository.findUserByUserId(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(passwordEncoder.encode(Mockito.anyString())).thenReturn(Mockito.anyString());
+
+        userService.changeUserPassword(userId, newPassword);
+
+        Mockito.verify(passwordEncoder).encode(newPassword);
+        Mockito.verify(userRepository).save(Mockito.any(User.class));
+    }
+
+    @Test
+    void test_changePassword_fail_userNotFound() {
+        String userId = "userId";
+        String newPassword = "newPassword";
+        Mockito.when(userRepository.findUserByUserId(Mockito.anyString())).thenReturn(Optional.empty());
+        
+        assertThrows(UsernameNotFoundException.class, () -> userService.changeUserPassword(userId, newPassword));
     }
 }
