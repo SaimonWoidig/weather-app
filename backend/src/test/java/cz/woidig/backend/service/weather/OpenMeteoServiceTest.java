49b2d8d2dd0ed91590deaf7db698a17bfab93312
Šimon Woidig <woidig.simon@gmail.com>
2024-05-21 00:23:01 +0200

    feat: partial tests for OpenMeteoService

--- backend/src/test/java/cz/woidig/backend/service/weather/OpenMeteoServiceTest.java
+++ backend/src/test/java/cz/woidig/backend/service/weather/OpenMeteoServiceTest.java
@@ -1,8 +1,10 @@
 package cz.woidig.backend.service.weather;
 
 import cz.woidig.backend.dto.weather.WeatherDTO;
+import cz.woidig.backend.dto.weather.WeatherDaysDTO;
 import cz.woidig.backend.dto.weather.openmeteo.OpenMeteoCurrent;
 import cz.woidig.backend.dto.weather.openmeteo.OpenMeteoDTO;
+import cz.woidig.backend.dto.weather.openmeteo.OpenMeteoDaily;
 import cz.woidig.backend.exceptions.WeatherException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,9 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -174,5 +179,87 @@ class OpenMeteoServiceTest {
         WeatherException expected = new WeatherException(expectedMessage);
         WeatherException actual = assertThrows(WeatherException.class, () -> openMeteoService.getCurrentWeather(latitude, longitude));
         assertEquals(expected.getMessage(), actual.getMessage());
+    }
+
+    @Test
+    public void test_get7DayForecast_success() {
+        float latitude = 0.0f;
+        float longitude = 0.0f;
+        int weatherCode = 1;
+
+        List<WeatherDTO> expectedWeathers = new ArrayList<>();
+        List<LocalDate> expectedDates = new ArrayList<>();
+        String[] dateStrings = {"2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05", "2024-01-06", "2024-01-07", "2024-01-08"};
+        Float[] temperatureMaxs = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f};
+        Float[] temperatureMins = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f};
+        Float[] precipitations = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f};
+        Integer[] weatherCodes = {weatherCode, weatherCode, weatherCode, weatherCode, weatherCode, weatherCode, weatherCode, weatherCode};
+
+        for (int i = 0; i < 7; i++) {
+            expectedWeathers.add(new WeatherDTO(temperatureMaxs[i + 1], temperatureMins[i + 1], weatherCodes[i + 1]));
+            expectedDates.add(LocalDate.parse(dateStrings[i + 1]));
+        }
+
+        OpenMeteoDaily meteoDaily = new OpenMeteoDaily(
+                dateStrings,
+                temperatureMaxs,
+                temperatureMins,
+                precipitations,
+                weatherCodes
+        );
+
+        OpenMeteoDTO requestData = new OpenMeteoDTO(null, meteoDaily);
+        ResponseEntity<OpenMeteoDTO> responseEntity = ResponseEntity.ofNullable(requestData);
+        Mockito.doReturn(responseEntity)
+                .when(mockRestTemplate).getForEntity(Mockito.any(URI.class), Mockito.eq(OpenMeteoDTO.class));
+        Mockito.doReturn(mockRestTemplate)
+                .when(mockRestTemplateBuilder).build();
+
+
+        WeatherDaysDTO expected = new WeatherDaysDTO(expectedWeathers, expectedDates);
+        WeatherDaysDTO actual = openMeteoService.get7DayForecast(latitude, longitude);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_getPrevious7DayForecast_success() {
+        float latitude = 0.0f;
+        float longitude = 0.0f;
+        int weatherCode = 1;
+
+        List<WeatherDTO> expectedWeathers = new ArrayList<>();
+        List<LocalDate> expectedDates = new ArrayList<>();
+        String[] dateStrings = {"2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05", "2024-01-06", "2024-01-07"};
+        Float[] temperatureMaxs = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+        Float[] temperatureMins = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+        Float[] precipitations = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+        Integer[] weatherCodes = {weatherCode, weatherCode, weatherCode, weatherCode, weatherCode, weatherCode, weatherCode};
+
+        for (int i = dateStrings.length - 1; i >= 0; i--) {
+            expectedWeathers.add(new WeatherDTO(temperatureMaxs[i], temperatureMins[i], weatherCodes[i]));
+            expectedDates.add(LocalDate.parse(dateStrings[i]));
+        }
+
+        OpenMeteoDaily meteoDaily = new OpenMeteoDaily(
+                dateStrings,
+                temperatureMaxs,
+                temperatureMins,
+                precipitations,
+                weatherCodes
+        );
+
+        OpenMeteoDTO requestData = new OpenMeteoDTO(null, meteoDaily);
+        ResponseEntity<OpenMeteoDTO> responseEntity = ResponseEntity.ofNullable(requestData);
+        Mockito.doReturn(responseEntity)
+                .when(mockRestTemplate).getForEntity(Mockito.any(URI.class), Mockito.eq(OpenMeteoDTO.class));
+        Mockito.doReturn(mockRestTemplate)
+                .when(mockRestTemplateBuilder).build();
+
+
+        WeatherDaysDTO expected = new WeatherDaysDTO(expectedWeathers, expectedDates);
+        WeatherDaysDTO actual = openMeteoService.get7PreviousDays(latitude, longitude);
+
+        assertEquals(expected, actual);
     }
 }
