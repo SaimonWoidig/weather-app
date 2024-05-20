99bce7f4ea0d0704a61689199a4ea51f96606051
Šimon Woidig <woidig.simon@gmail.com>
2024-05-20 21:33:33 +0200

    feat: handle method not allowed

--- backend/src/test/java/cz/woidig/backend/controller/ControllerExceptionHandlerTest.java
+++ backend/src/test/java/cz/woidig/backend/controller/ControllerExceptionHandlerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -86,6 +87,16 @@ class ControllerExceptionHandlerTest {
 
         ErrorDTO expected = new ErrorDTO(401, "Invalid password");
         ErrorDTO actual = controllerExceptionHandler.handleInvalidPasswordException(e, null);
+
+        assertEquals(expected.message(), actual.message());
+    }
+
+    @Test
+    void test_handleHttpRequestMethodNotSupportedException() {
+        HttpRequestMethodNotSupportedException e = new HttpRequestMethodNotSupportedException("TEST");
+
+        ErrorDTO expected = new ErrorDTO(405, "Request method 'TEST' is not supported");
+        ErrorDTO actual = controllerExceptionHandler.handleHttpRequestMethodNotSupportedException(e, null);
 
         assertEquals(expected.message(), actual.message());
     }
